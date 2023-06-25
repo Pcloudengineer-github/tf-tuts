221fbfcab205e2a9931f14f7f86a8baa7c58a54e
Pcloudengineer-github <129958230+Pcloudengineer-github@users.noreply.github.com>
2023-06-25 11:24:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
 resource "aws_instance" "my_vm" {
  ami           = var.ami //Ubuntu AMI
  instance_type = var.instance_type
- region = "us-east-1"
+ 
 
  tags = {
    Name = var.name_tag,
