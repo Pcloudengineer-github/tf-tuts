ff05d4c11730c157fa167f465421cc0f4c3419de
Pcloudengineer-github <129958230+Pcloudengineer-github@users.noreply.github.com>
2023-06-25 11:28:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "ami" {
    type        = string
-   description = "Ubuntu AMI ID in N. Virginia Region"
-   default     = "ami-065deacbcaac64cf2"
+   description = "linux AMI ID in N. Virginia Region"
+   default     = "ami-022e1a32d3f742bd8"
 }
 
 variable "instance_type" {
